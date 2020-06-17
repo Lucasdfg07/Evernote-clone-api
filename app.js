cd2f28ec0fcd0d678c4c5fa73e64e82b27c3e978
Lucas Siqueira Fernandes <lucassiqueirafernandes@MacBook-Air-de-Lucas.local>
2020-06-16 21:30:31 -0300

    Setting URL Allowed By Cors

--- app.js
+++ app.js
@@ -17,6 +17,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
 
-app.use(cors());
+// Set up a whitelist and check against it:
+var whitelist = ['http://localhost:3000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 module.exports = app;
